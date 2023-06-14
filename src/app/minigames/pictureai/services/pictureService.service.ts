5ef14d8156d4ed1bfeafb85f40d533ba1b7f993e
RubenKraaijeveld <ruben01@outlook.com>
2023-06-14 14:24:41 +0200

    added otter description

--- src/app/minigames/pictureai/services/pictureService.service.ts
+++ src/app/minigames/pictureai/services/pictureService.service.ts
@@ -8,7 +8,7 @@ export class PictureService implements IPictureService {
         new Picture({
             name: "Otter with ball",
             image: "assets/AiPictures/otterWithBall.jpg",
-            description: "TBD"
+            description: "This image shows a sea otter swimming in a pool with a blue ball in its mouth. The otter is floating on its back with its flippers in the water. The otter’s fur is dark brown and its eyes are large and black. The otter’s body is slender and its head is round. The ball is blue and has a small hole in the middle. The pool is clear and blue with a few small bubbles floating in it. The water is calm and still. The otter’s body is submerged in the water except for its head and flippers. The otter’s body is slender and its head is round. The ball is blue and has a small hole in the middle. The pool is clear and blue with a few small bubbles floating in it. The water is calm and still."
         }),
         new Picture({
             name: "Evil AI",
