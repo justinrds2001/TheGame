ae9976bcf9a1cdd3f1dac1b535391e16354bc31c
lars <ljaw.jongenelen@student.avans.nl>
2023-06-14 14:25:11 +0200

    added dialog/clicks

--- src/app/minigames/pictureai/pictureai.component.ts
+++ src/app/minigames/pictureai/pictureai.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { IPictureService } from "./services/IPictureService.service";
 import { Picture } from "./models/picture.model";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   templateUrl: "./pictureai.component.html",
@@ -11,8 +12,11 @@ export class PictureaiComponent implements OnInit
 {
 	picture: Picture = new Picture({name: '', image: '', description: ''});
 	showAiAnswer: boolean = false;
+    disable: boolean = false;
 
-  constructor(private pictureService: IPictureService) {}
+  constructor(public dialog: MatDialog, private pictureService: IPictureService) {
+    this.openDialog();
+  }
 
   ngOnInit(): void {
     this.pictureService.resetPictureCounter();
@@ -20,4 +24,31 @@ export class PictureaiComponent implements OnInit
       .getRandomPicture()
       .subscribe((picture) => (this.picture = picture));
   }
+
+  submit(): void {
+    this.showAiAnswer = true;
+    this.disable = true;
+  }
+
+  next(): void {
+    this.pictureService.getRandomPicture().subscribe((picture) => (this.picture = picture));
+    this.showAiAnswer = false;
+    this.disable = false;
+    if (this.picture.description == "Information") {
+      this.disable = true;
+      this.showAiAnswer = true;
+    }
+  }
+
+  openDialog() {
+    this.dialog.open(Dialog);
+  }
+  
 }
+
+@Component({
+    selector: "dialog-pictureai",
+    templateUrl: "./dialog-pictureai.html",
+  })
+  export class Dialog {}
+  
