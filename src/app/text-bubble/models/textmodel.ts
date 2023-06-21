c575cba743a1c025416e0b477996d058854c60ae
Diren Ozturk <od.ozturk@student.avans.nl>
2023-06-21 16:34:34 +0200

    DrawAI test-conversation added with clicking functionality

---
+++ src/app/text-bubble/models/textmodel.ts
@@ -0,0 +1,5 @@
+import { Player } from "./playermodel";
+
+export class TextModel {
+  constructor(public player: Player, public text: string) {}
+}
--- src/app/text-bubble/text-bubble.component.ts
+++ src/app/text-bubble/text-bubble.component.ts
@@ -1,8 +1,57 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
+import { Player } from "./models/playermodel";
+import { TextModel } from "./models/textmodel";
 
 @Component({
   selector: "app-text-bubble",
   templateUrl: "./text-bubble.component.html",
   styleUrls: ["./text-bubble.component.css"],
 })
-export class TextBubbleComponent {}
+export class TextBubbleComponent implements OnInit {
+  sophia: Player = new Player("Sophia", "assets/images/sophia.png");
+  nyx: Player = new Player("Nyx", "assets/images/nyx-good.png");
+  cssClass: string = "player";
+  drawAiConversation: TextModel[] = [
+    {
+      player: this.sophia,
+      text: "Finally. I have finished the AI model that I have been working on for so long.",
+    },
+    {
+      player: this.sophia,
+      text: "I can't wait to see how it performs.",
+    },
+    {
+      player: this.sophia,
+      text: "I will just run it on my computer and see what happens.",
+    },
+    {
+      player: this.nyx,
+      text: "Hello, dr. Sophia Grey. I am Nyx.",
+    },
+    {
+      player: this.nyx,
+      text: "Thank you for creating me.",
+    },
+  ];
+  currentTextModel: TextModel | undefined;
+  iterator: number = 0;
+
+  ngOnInit(): void {
+    this.currentTextModel = this.drawAiConversation[this.iterator];
+  }
+
+  @HostListener("click", ["$event"])
+  onClick(_event: MouseEvent) {
+    this.iterator++;
+    if (this.iterator < this.drawAiConversation.length) {
+      this.currentTextModel = this.drawAiConversation[this.iterator];
+      if (this.currentTextModel.player.playerName == "Sophia") {
+        this.cssClass = "player";
+      } else {
+        this.cssClass = "npc";
+      }
+    } else {
+      //Close the text bubble
+    }
+  }
+}
